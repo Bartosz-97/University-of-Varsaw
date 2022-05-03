862b2b1357d6519cbb959c7f6b00b0c004c4fd2a
Bartosz Kaczmarek <kaczmarek.b97@gmail.com>
2022-05-03 22:57:06 +0200

    Add script manaul slider

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,9 @@ const burger = document.querySelector('nav .burger');
 const h1 = document.querySelector('header .slider');
 const blueH1 = document.querySelector('header .slider-blue');
 
+const leftArrow = document.querySelector('header .fa-arrow-left');
+const rightArrow = document.querySelector('header .fa-arrow-right');
+
 const slideList = [{
   text1: 'Będziesz uczyć się z ciekawością.',
   text2: 'Obiecujemy'
@@ -18,10 +21,15 @@ const slideList = [{
 let active = 0;
 const time = 5000;
 
+let arrowL = true;
+
 const burgerActive = () => {
   burger.classList.toggle('change');
   burger.classList.toggle('active');
 }
+
+burger.addEventListener('click', burgerActive);
+
 
 const changeSlide = () => {
  active++;
@@ -31,6 +39,21 @@ const changeSlide = () => {
  h1.textContent = slideList[active].text1;
  blueH1.textContent = slideList[active].text2;
 }
-setInterval(changeSlide, time);
+let indexInterval = setInterval(changeSlide, time);
 
-burger.addEventListener('click', burgerActive)
+const manualSlide = () => {
+  if (arrowL) {
+    clearInterval(indexInterval);
+    active --;
+    if (active === slideList.length) {
+      active = 0;
+    } else if (active < 0) {
+      active = slideList.length -1;
+    }
+  } 
+  h1.textContent = slideList[active].text1;
+  blueH1.textContent = slideList[active].text2;
+}
+
+leftArrow.addEventListener('click', manualSlide);
+rightArrow.addEventListener('click', manualSlide);
--- static/js/menu.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector('nav .burger');
-
-burger.addEventListener('click', function() {
-  burger.classList.toggle('change');
-  burger.classList.toggle('active');
-})
