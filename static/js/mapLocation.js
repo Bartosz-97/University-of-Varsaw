4a180ab2d37fd2afafa29fd16e642acaf5a0e906
Bartosz Kaczmarek <kaczmarek.b97@gmail.com>
2022-05-08 22:48:20 +0200

    add script location map

---
+++ static/js/mapLocation.js
@@ -0,0 +1,15 @@
+function initMap() {
+  const myLocation = { lat: 52.37608592648658, lng: 16.871996869770882 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: myLocation,
+    zoomControl: false,
+  });
+
+  const marker = new google.maps.Marker({
+    position: myLocation,
+    map: map,
+    icon: "/home/bartosz/Desktop/Strona_rekrutacyjna/static/image/marker.png"
+  });
+
+}
